f98ca2f184290f9c06abf586597065088fa4ae96
xn--mical vanhalst-8kb <mvanhalst@sfr.fr>
2014-10-31 09:31:12 +0000

    with variable

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-echo "Hello Mike!";
+$title = 'Hello Mike!';
 
 ?>
+
+<html>
+	<head>
+		<title><?php echo $title; ?></title>
+	</head>
+	<body>
+		<h1><?php echo $title; ?></title>
+	</body>
+</html>
