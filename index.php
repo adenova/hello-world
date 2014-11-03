bbc878f4147e0116afd0eb0e578a27bc54e7f23f
adenova <agence@adenova.fr>
2014-11-03 21:15:52 +0100

    with var

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
-echo "Hello World!";
+$title = "Hello World!";
 
 ?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><title><?php echo $title; ?></title></head>
+<body>
+    <h1><?php echo $title; ?></h1>
+</body>
+</html>
