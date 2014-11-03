08f5ccac48d1d8eb052b5d93dc62cf61be853950
adenova <agence@adenova.fr>
2014-11-03 22:00:19 +0100

    init

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$title = "Hello World!";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title><?php echo $title; ?></title>
+    </head>
+    <body>
+        <h1><?php echo $title; ?></h1>
+    </body>
+</html>
